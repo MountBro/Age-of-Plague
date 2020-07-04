f4d43fa15b5f4e53943467cb8f6f7c2f74d3d045
huyuchun <ljg16654@sjtu.edu.cn>
2020-07-04 14:57:23 +0800

    used cell of tiles in class city

--- simsrc/tt.m
+++ simsrc/tt.m
@@ -2,13 +2,22 @@ c = city (4,4, 200);
 
 for i = 1:3
     for j = 1:4
-        t = tile(2 * i - j, i + 3*j, 20);
-        t.infected = mod (5* i + 13*j, 21);
-        c.tiles = [c.tiles, t]
+        if (i ~= 2 || j ~= 2)
+            c.tilesIndex = [c.tilesIndex; [i, j]];
+            t = tile(2 * i - j, i + 3*j, 20);  
+            t.infected = mod (5* i + 13*j, 21); % initialize infected population
+            c.tiles{i, j} = t;
+        end
     end
 end
 
-for i = 1:(length(c.tiles)-1)
-    c.tiles(i).render
+c
+
+sizeOfIndex = size(c.tilesIndex);
+numTiles = sizeOfIndex(1);
+for k = 1:numTiles
+    index = c.tilesIndex(k, :);
+    i = index(1); j = index(2);
+    c.tiles{i, j}.render;
 end
 

ba69c5662f2a52c74619cf76bd405dab4b3125c4
EthanJia <181716254@qq.com>
2020-07-04 18:18:03 +0800

    fetxh all the files

--- simsrc/exp.m
+++ simsrc/exp.m
@@ -1,18 +1,1 @@
 % This is the flag to stop the programs
-flag = false;
-
-while flag == true
-    
-
-
-
-% judge if to stop
-flag = judge;
-end
-
-
-function newFlag = judge()
-if condition == true
-    newFlag = false;
-end
-end
--- simsrc/virus.m
+++ simsrc/virus.m
@@ -1,10 +1,8 @@
 classdef virus
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
-    
+
     properties
         stat
-        % pos
+        name
 
 
     end
@@ -14,63 +12,52 @@ classdef virus
         function obj = virus()
             obj.stat = [];
         end
-        
-        % function survFlag = judgeSurv(stat)
-        %     survFlag = (status - 2) > 0 ; 
-        % end
 
         % change the virus status according to its neighbour numbers
         function obj = change()
             for i = 1:height(obj.stat)
-                obj.stat(i,1) = obj.stat(i,2) > 2 ;
+                if ~judgeAlive(obj.stat(i,:))
+                    obj.stat(i,:)=[];
+                end
             end
         end
 
         % this function calculates its neighnours' virus number
         function nb = calNB(x,y)
-            % x1=abs(x);
-            % y1=abs(y);
-            % if (abs(x) > 1) && (abs(y) > 1) 
             nb(1,:) = [x-1,y];
             nb(2,:) = [x-1,y+1];
             nb(3,:) = [x,y-1];
             nb(4,:) = [x,y+1];
             nb(5,:) = [x+1,y-1];
             nb(6,:) = [x+1,y];
-            % elseif x1 == 1 && y1 > 1 
-            %     nb(1,:) = [x,y-1];
-            %     nb(2,:) = [x,y+1];
-            %     nb(3,:) = [x+1,y-1];
-            %     nb(4,:) = [x+1,y];
-            % elseif x1 > 1 && y1 == 1 
-            %     nb(1,:) = [x-1,y];
-            %     nb(2,:) = [x-1,y+1];
-            %     % nb(3,:) = [x,y-1];
-            %     nb(3,:) = [x,y+1];
-            %     % nb(5,:) = [x+1,y-1];
-            %     nb(4,:) = [x+1,y];
-            % elseif x1 == 1 && y1 == 1 
-            %     % nb(1,:) = [x-1,y];
-            %     % nb(2,:) = [x-1,y+1];
-            %     % nb(3,:) = [x,y-1];
-            %     nb(1,:) = [x,y+1];
-            %     % nb(5,:) = [x+1,y-1];
-            %     nb(2,:) = [x+1,y];
-            % end
         end
 
-        function obj = deriveNB()
-            n = mapsz;
+        % check whether a hex is a member of virus
+        function res = ismember(pos)
             for i = 1:height(obj.stat)
-               nb = calNB(obj.stat(i,3),obj.stat(i,4));
-               x = obj.stat(i,3);
-               y = obj.stat(i,4);
-               for j = 1:6
-                   if obj.stat( ( (n + 1) * nb(j,1) + 1 + nb(j,2) ) , 1) == true
-                        obj.stat(i,2) = obj.stat(i,2) + 1;
-                   end
+                if pos == obj.stat(i,:)
+                    break
                 end
+            end
+            res = ~(i == height(obj.stat));
+        end
 
+
+        function res = judgeAlive(x,y)
+
+            % calculate the nb number
+            n = 1;
+            for i = 1:6
+                if ismember(calNB(x,y))
+                    n = n + 1;
+                end
+            end
+
+            % judge alive or not
+            if n >= 3
+                res = true;
+            else
+                res = false;
             end
 
         end

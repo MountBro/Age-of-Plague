a64dcbd86675a50e0af6808893473ea51f371e8b
EthanJia <181716254@qq.com>
2020-07-21 22:03:52 +0800

    [UI] #764
    - Change the main page layout

--- src/ViewHome.elm
+++ src/ViewHome.elm
@@ -10,11 +10,11 @@ import Message as M exposing (..)
 
 viewHome : List (Html Msg)
 viewHome =
-    [ H.p []
-        [ H.text "Age of Plague:"
+    [ H.p [ id "t1" ]
+        [ H.text "Age of Plague"
         , br [] []
-        , H.text "After the Apocalypse"
         ]
+    , H.p [ id "t2" ] [ H.text "After the Apocalypse" ]
     , div [ HA.id "ctnr" ]
         [ H.a [ HE.onClick (M.Click "startGame") ] [ H.text "Start the Game" ]
         , H.a [ HE.onClick (M.Click "card") ] [ H.text "Card Gallery" ]
